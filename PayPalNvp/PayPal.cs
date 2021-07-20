ab7cfb39b863e4c934370ba72cec6253af5bf66c
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-07-20 16:25:00 -0500

    Add CancellationToken to all async methods

--- PayPalNvp/PayPal.cs
+++ PayPalNvp/PayPal.cs
@@ -66,7 +66,7 @@ namespace NeoSmart.PayPalNvp
             string method = "DoExpressCheckoutPayment";
             return GenericNvp(method, fields);
         }
-        
+
         public Dictionary<string, string> RefundTransaction(Dictionary<string, string> fields)
         {
             string method = "RefundTransaction";
--- PayPalNvp/PayPalAsync.cs
+++ PayPalNvp/PayPalAsync.cs
@@ -2,25 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NeoSmart.PayPalNvp
 {
     public partial class PayPal
     {
-        private async Task<string> PostAsync(string url, string postData, string contentType = "application/x-www-form-urlencoded")
+        private Task<string> PostAsync(string url, string postData, CancellationToken cancel)
+        {
+            return PostAsync(url, postData, cancel: default);
+        }
+
+        private async Task<string> PostAsync(string url, string postData, string contentType = "application/x-www-form-urlencoded", CancellationToken cancel = default)
         {
             using (var client = new System.Net.Http.HttpClient())
             using (var httpContent = new System.Net.Http.StringContent(postData, Encoding.UTF8, contentType))
-            using (var response = await client.PostAsync(url, httpContent))
+            using (var response = await client.PostAsync(url, httpContent, cancel))
             {
                 return await response.Content.ReadAsStringAsync();
             }
         }
 
-        public async Task<Dictionary<string, string>> GenericNvpAsync(string method, Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> GenericNvpAsync(string method, Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            //Add some default PayPal-specific values
+            // Add some default PayPal-specific values
             fields["USER"] = _user;
             fields["PWD"] = _password;
             fields["SIGNATURE"] = _signature;
@@ -29,59 +35,59 @@ namespace NeoSmart.PayPalNvp
 
             string nvpstr = EncodeNvpString(fields);
 
-            //Send the POST request to PayPal
-            var response = await PostAsync(NvpEndPoint, nvpstr);
+            // Send the POST request to PayPal
+            var response = await PostAsync(NvpEndPoint, nvpstr, cancel);
 
             return DecodeNvpString(response);
         }
 
-        //Start of PayPal convenience methods
-        public async Task<Dictionary<string, string>> SetExpressCheckoutAsync(Dictionary<string, string> fields)
+        // Start of PayPal convenience methods
+        public async Task<Dictionary<string, string>> SetExpressCheckoutAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "SetExpressCheckout";
-            return await GenericNvpAsync(method, fields);
+            const string method = "SetExpressCheckout";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> GetExpressCheckoutDetailsAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> GetExpressCheckoutDetailsAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "GetExpressCheckoutDetails";
-            return await GenericNvpAsync(method, fields);
+            const string method = "GetExpressCheckoutDetails";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> DoExpressCheckoutPaymentAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> DoExpressCheckoutPaymentAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "DoExpressCheckoutPayment";
-            return await GenericNvpAsync(method, fields);
+            const string method = "DoExpressCheckoutPayment";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> RefundTransactionAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> RefundTransactionAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "RefundTransaction";
-            return await GenericNvpAsync(method, fields);
+            const string method = "RefundTransaction";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> DoDirectPaymentAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> DoDirectPaymentAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "DoDirectPayment";
-            return await GenericNvpAsync(method, fields);
+            const string method = "DoDirectPayment";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> DoCaptureAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> DoCaptureAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "DoCapture";
-            return await GenericNvpAsync(method, fields);
+            const string method = "DoCapture";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> DoVoidAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> DoVoidAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "DoVoid";
-            return await GenericNvpAsync(method, fields);
+            const string method = "DoVoid";
+            return await GenericNvpAsync(method, fields, cancel);
         }
 
-        public async Task<Dictionary<string, string>> GetTransactionDetailsAsync(Dictionary<string, string> fields)
+        public async Task<Dictionary<string, string>> GetTransactionDetailsAsync(Dictionary<string, string> fields, CancellationToken cancel = default)
         {
-            string method = "GetTransactionDetails";
-            return await GenericNvpAsync(method, fields);
+            const string method = "GetTransactionDetails";
+            return await GenericNvpAsync(method, fields, cancel);
         }
     }
 }
